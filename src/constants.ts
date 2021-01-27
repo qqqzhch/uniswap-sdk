8b1cdcf207ee0bc5b48364391796a72f0a94c281
zhangchao <544262408@qq.com>
2021-01-27 14:28:30 +0800

    test network

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x7c398c83865d36d2b61a38f35297dd0bdf19aafd'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
